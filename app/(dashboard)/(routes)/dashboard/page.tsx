93d633576baab51636c6272924e1ffc4105ad547
Facundo Godoy <facu14_23@hotmail.com>
2023-10-15 00:55:42 -0300

    Clerk Authentication

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <p>dashboard page (Protected)</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default DashboardPage;
